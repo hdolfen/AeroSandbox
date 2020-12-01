bd1d4ec89a4f2b133362f2b7a0c18accf285afb7
hdolfen <52496779+hdolfen@users.noreply.github.com>
2020-12-01 13:58:55 +0100

    Function to read a variable from pickle if it exists

--- examples/Project_2/solutio2_0.py
+++ examples/Project_2/solutio2_0.py
@@ -8,6 +8,17 @@ from examples.Project_2.glidersimulator2 import GliderSimulator
 from examples.optimtools import Simulator
 from pyDOE2 import fullfact
 
+
+def read_from_pickle(var):
+    file = Path(str(var) + ".pickle")
+    if file.exists():
+        with open(file, "rb") as f:
+            var = pickle.load(f)
+        return True
+    else:
+        return False
+
+
 glider_simulator = GliderSimulator()
 x0 = [0.2, 0.2, 0.2]
 names = ["y_mid_1", "chord_mid_1", "chord_mid_2", "chord_mid_3"]
